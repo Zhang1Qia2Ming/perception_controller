5966bbcdf992c8c335bef459396b0d0cee34a1d9
Zhang1Qia2Ming <1619140145@qq.com>
2026-02-13 09:18:27 +0000

    add generate_param_lib and compile passed version

--- include/perception_system_controller/perception_system_controller.hpp
+++ include/perception_system_controller/perception_system_controller.hpp
@@ -1,10 +1,22 @@
 #ifndef PERCEPTION_CONTROLLER__PERCEPTION_SYSTEM_CONTROLLER_HPP_
 #define PERCEPTION_CONTROLLER__PERCEPTION_SYSTEM_CONTROLLER_HPP_
 
+#include <atomic>
+#include <thread>
+#include <map>
+#include <algorithm>
+#include <iterator>
+
 #include "controller_interface/controller_interface.hpp"
+#include "hardware_interface/loaned_state_interface.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "cv_bridge/cv_bridge.h"
 #include <sensor_msgs/msg/image.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <perception_controller/perception_system_controller_params_lib.hpp>
+
 
 namespace perception_system_controller {
 
@@ -32,8 +44,33 @@ class PerceptionSystemController : public controller_interface::ControllerInterf
             const rclcpp::Time& time, const rclcpp::Duration& period) override;
         
     protected:
-        std::shared_ptr<perception_system_controller::ParamListener> param_listener_;
-        perception_system_controller::Params params_;
+        struct BaseMember {
+            std::string name;
+            std::string device_type;
+            std::vector<std::string> sensor_components;
+            std::string interface_name;
+            std::string frame_id;
+            bool enable = false;
+
+            using ImagePub = rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::Image>::SharedPtr;
+            using ImuPub = rclcpp_lifecycle::LifecyclePublisher<sensor_msgs::msg::Imu>::SharedPtr;
+            using PosePub = rclcpp_lifecycle::LifecyclePublisher<geometry_msgs::msg::PoseStamped>::SharedPtr;
+            
+            ImagePub pub_raw;
+            ImagePub pub_rect;
+            ImuPub pub_imu;
+            PosePub pub_pose;
+        };
+        void worker_thread(std::shared_ptr<BaseMember> member, size_t interface_idx);
+        
+        std::vector<std::shared_ptr<BaseMember>> members_;
+
+        std::shared_ptr<ParamListener> param_listener_;
+        Params params_;
+
+        // about thread
+        std::vector<std::thread> threads_;
+        std::atomic<bool> is_running_{false};
 };
 
 } // namespace perception_system_controller
--- src/perception_system_controller.cpp
+++ src/perception_system_controller.cpp
@@ -1,7 +1,8 @@
 #include "perception_system_controller/perception_system_controller.hpp"
-#include "perception_hardware/mock_camera_data.hpp"
+#include "mock_device/mock_camera_data.hpp"
 #include "controller_interface/helpers.hpp"
 
+#include <map>
 
 namespace perception_system_controller {
     PerceptionSystemController::PerceptionSystemController() : controller_interface::ControllerInterface() {}
@@ -26,25 +27,127 @@ namespace perception_system_controller {
         RCLCPP_INFO(get_node()->get_logger(), "PerceptionSystemController on_configure");
         params_ = param_listener_->get_params();
 
-        
-        
+        members_.clear();
+
+        for(auto const& [name, member] : params_.members.device_list_map)
+        {   
+            auto member_ptr = std::make_shared<BaseMember>();
+            member_ptr->name = name;
+            member_ptr->device_type = member.device_type;
+            member_ptr->enable = member.enable;
+            member_ptr->sensor_components = member.sensor_components;
+            member_ptr->interface_name = member.interface_name;
+            member_ptr->frame_id = member.frame_id;
+
+            for(const auto & sensor_component : member.sensor_components) {
+                if (sensor_component == "image")
+                {
+                    member_ptr->pub_raw = get_node()->create_publisher<sensor_msgs::msg::Image>(member.topic_raw, 10);
+                    member_ptr->pub_rect = get_node()->create_publisher<sensor_msgs::msg::Image>(member.topic_rect, 10);
+                }
+                else if (sensor_component == "pose")
+                {
+                    member_ptr->pub_pose = get_node()->create_publisher<geometry_msgs::msg::PoseStamped>(member.topic_pose, 10);
+                }
+                else if (sensor_component == "imu")
+                {
+                    member_ptr->pub_imu = get_node()->create_publisher<sensor_msgs::msg::Imu>(member.topic_imu, 10);
+                }            
+                
+            }
+            members_.push_back(member_ptr);
+
+            RCLCPP_INFO(get_node()->get_logger(), "PerceptionSystemController on_configure member: %s", name.c_str());
+        }
+
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
     controller_interface::CallbackReturn PerceptionSystemController::on_activate(const rclcpp_lifecycle::State& previous_state)
     {
+        is_running_ = true;
+
+        for(auto & member : members_) {
+            auto it = std::find_if(
+                state_interfaces_.begin(),
+                state_interfaces_.end(),
+                [&](const hardware_interface::LoanedStateInterface & si) {
+                    return si.get_interface_name() == member->interface_name;
+                });
+            
+            if(it != state_interfaces_.end()) {
+                size_t idx = std::distance(state_interfaces_.begin(), it);
+
+                if(member->enable) {
+                    threads_.emplace_back(&PerceptionSystemController::worker_thread, this, member, idx);
+                    RCLCPP_INFO(get_node()->get_logger(), "PerceptionSystemController on_activate member: %s", member->name.c_str());
+                }
+            }
+            
+        }
+
         return controller_interface::CallbackReturn::SUCCESS;
+    }
+
+    controller_interface::InterfaceConfiguration PerceptionSystemController::state_interface_configuration() const
+    {
+        controller_interface::InterfaceConfiguration config;
+        config.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+        
+        for(const auto & member : members_)
+        {
+            config.names.push_back(member->interface_name);
+        }
+        return config;
+    }
+
+    controller_interface::InterfaceConfiguration PerceptionSystemController::command_interface_configuration() const
+    {
+        return controller_interface::InterfaceConfiguration{controller_interface::interface_configuration_type::NONE,
+        };
     }
 
     controller_interface::CallbackReturn PerceptionSystemController::on_deactivate(const rclcpp_lifecycle::State& previous_state)
     {
+        is_running_ = false;
+        for(auto & thread : threads_) {
+            if(thread.joinable()) {
+                thread.join();
+            }
+        }
+        threads_.clear();
+        
         return controller_interface::CallbackReturn::SUCCESS;
     }
 
-    controller_interface::CallbackReturn PerceptionSystemController::update(const rclcpp::Time& time, const rclcpp::Duration& period)
+    controller_interface::return_type PerceptionSystemController::update(const rclcpp::Time& time, const rclcpp::Duration& period)
     {
-        return controller_interface::CallbackReturn::SUCCESS;
+        return controller_interface::return_type::OK;
     }
 
+    void PerceptionSystemController::worker_thread(std::shared_ptr<BaseMember> member, size_t interface_idx)
+    {
+        auto logger = get_node()->get_logger();
+
+        while(is_running_ && rclcpp::ok())
+        {
+            // get raw ptr
+            double raw_val = state_interfaces_[interface_idx].get_value();
+            uintptr_t ptr_address = static_cast<uintptr_t>(raw_val);
+
+            if (ptr_address == 0) {
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                continue;
+            }
+
+            // distrubute data
+            if(member->device_type == "mock_camera")
+            {
+                auto mock_camera_data = reinterpret_cast<perception_hardware::MockCameraData*>(ptr_address);
+
+
+            }
+        }
+    }
 
 } // namespace perception_system_controller
